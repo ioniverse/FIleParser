6ca863468c767f2a4f4826601f20eb22dfc7ac86
philippe <yoniphilippe@gmail.com>
2014-09-15 09:23:46 +0200

    question 1 realise

--- src/scm/Example.java
+++ src/scm/Example.java
@@ -1,18 +1,46 @@
 package scm;
 
-public class Example implements ContentHandler{
-	public FileParser entree;
+import java.io.File;
 
-	
 
-	@Override
-	public void defaultLine(String content) {
+public class Example {
+	public static FileParser entree;
+	static int compteur= 0;
+
+	public class Compteur implements ContentHandler
+	{
+		public int compteur;
+		
+		public Compteur()
+		{
+			compteur=0;
+		}
+		@Override
+		public void defaultLine(String content) {
+			
+			this.compteur++;
+		}
 		
 		
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+	public static void main(String[] args) 
+	{
+		
+		
+		File file= new File("./src/monfichier.txt");
+		
+
+		entree= new FileParser();
+		entree.parse(file, new ContentHandler() {
+			
+			@Override
+			public void defaultLine(String content) {
+				compteur++;
+				
+			}
+		});
+		System.out.println("Le monfichier.txt contient "+compteur+" ligne(s)");
 
 	}
 
--- src/scm/FileParser.java
+++ src/scm/FileParser.java
@@ -1,12 +1,41 @@
 package scm;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class FileParser {
 
+	InputStream fis;
+	InputStreamReader isr;
+	BufferedReader br;
+	String line=null;
 	public void parse(File file, ContentHandler handler)
 	{
+		try {
+			fis= new FileInputStream(file);
+			isr= new InputStreamReader(fis);
+			br= new BufferedReader(isr);
+			
+			while ((line=br.readLine())!=null)
+				handler.defaultLine(line);
+			br.close();
+		} 
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 		
 	}
 	
